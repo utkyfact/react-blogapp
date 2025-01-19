1604d336d520492de88256e2961f668e8c584f4c
Utku Şen <utkusen9@gmail.com>
2025-01-19 23:30:37 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router', 'react-loader-spinner'],
-      output: {
-        globals: {
-          'react-router': 'ReactRouter',
-          'react-loader-spinner': 'ReactLoaderSpinner'
-        }
-      }
-    }
-  },
-  optimizeDeps: {
-    include: ['react-loader-spinner', 'react-router']
-  }
 })
